dcd20e6f4ed8acc45bd9bcdb1cb65617e6848936
laurie <laurie@brandwatch.com>
2020-02-05 15:03:26 +0000

    Add Flex and some examples

---
+++ src/Flex/Flex.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+
+import "./flex.css";
+
+export function Flex({
+  children,
+  direction,
+  justifyContent,
+  className,
+  alignItems,
+  wrap,
+  Component = "div",
+  ...rest
+}) {
+  const classes = classnames(className, "flex-container", {
+    [`flex-justify-content-${justifyContent}`]: justifyContent,
+    [`flex-${direction}`]: direction,
+    [`flex-align-items-${alignItems}`]: alignItems,
+    [`flex-wrap-${wrap}`]: wrap
+  });
+  return (
+    <Component className={classes} {...rest}>
+      {children}
+    </Component>
+  );
+}
+
+Flex.propTypes = {
+  alignItems: PropTypes.oneOf(["stretch", "flex-start", "flex-end", "center"]),
+  direction: PropTypes.oneOf([
+    "row",
+    "column",
+    "row-reverse",
+    "column-reverse"
+  ]),
+  justifyContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly"
+  ]),
+  wrap: PropTypes.oneOf(["wrap", "no-wrap", "wrap-reverse"])
+};
