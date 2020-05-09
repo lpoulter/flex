630b1f3052dd2c25072680d3f8df08e7958d69ed
laurie <laurie@brandwatch.com>
2020-05-09 12:02:27 +0100

    fix: Only apply flex style if they are defined

--- Flex/Base.js
+++ Flex/Base.js
@@ -1,28 +1,27 @@
 import React from "react";
 
 export function Base({
-         As = "div",
-         className,
-         children,
-         grow,
-         basis,
-         shrink,
-         style,
-         flex,
-         ...rest
-       }) {
-         return (
-           <As
-             className={className}
-             {...rest}
-             style={{
-               ...style,
-               flexGrow: grow,
-               flexBasis: basis,
-               flexShrink: shrink,
-             }}
-           >
-             {children}
-           </As>
-         );
-       }
+  As = "div",
+  className,
+  children,
+  grow,
+  basis,
+  shrink,
+  style,
+  flex,
+  ...rest
+}) {
+  const elementStyle = {
+    ...style,
+    ...(grow && { flexGrow: grow }),
+    ...(basis && { flexBasis: basis }),
+    ...(shrink && { flexShrink: shrink }),
+    ...(flex && { flex }),
+  };
+  
+  return (
+    <As className={className} {...rest} style={elementStyle}>
+      {children}
+    </As>
+  );
+}
